a81622395dbc389ebb98f17e5fe4beca8dc7c332
moritzgoeckel <moritz.goeckel@gmail.com>
2016-10-21 05:14:43 +0900

    Implemented the optimize percentage method

--- V3-Trader-Project/Trader/Application/IndicatorOptimizer.cs
+++ V3-Trader-Project/Trader/Application/IndicatorOptimizer.cs
@@ -38,8 +38,11 @@ namespace V3_Trader_Project.Trader.Application
             if (running == true)
                 throw new Exception("Already running!");
 
-            if(outcomeCodes == null || double.IsNaN(outcomeCodePercent))
-                findOutcomeCode(0.5);
+            /*if(outcomeCodes == null || double.IsNaN(outcomeCodePercent))
+                findOutcomeCodeForDesiredDistribution(0.5);*/
+
+            if (outcomeCodes == null || double.IsNaN(outcomeCodePercent))
+                optimizeOutcomeCodePercentage(300);
 
             Logger.log("Start testing indicators");
             new Thread(delegate () {
@@ -56,7 +59,7 @@ namespace V3_Trader_Project.Trader.Application
             running = false;
         }
 
-        private void findOutcomeCode(double desiredDistribution)
+        private void findOutcomeCodeForDesiredDistribution(double desiredDistribution)
         {
             Logger.log("Find outcome percent for " + desiredDistribution);
             double successRatio;
@@ -89,7 +92,7 @@ namespace V3_Trader_Project.Trader.Application
                 else if(score < desiredDistribution - desiredDistributionTolerance)
                     outcomeCodePercent -= (outcomeCodePercent / (10 + round));
 
-                Logger.log("TRY OPT. Round " + round + " -> " + outcomeCodePercent + "% = b" + Math.Round(buyDist, 4) + " s" + Math.Round(sellDist, 4) + " =" + Math.Round(score, 4));
+                Logger.log("SetDist OPT. Round " + round + " -> " + outcomeCodePercent + "% = b" + Math.Round(buyDist, 4) + " s" + Math.Round(sellDist, 4) + " =" + Math.Round(score, 4));
 
                 round++;
             }
@@ -97,9 +100,52 @@ namespace V3_Trader_Project.Trader.Application
             Logger.log("SATTLE OPT. dist for "+ outcomeCodePercent + "% at b" + Math.Round(buyDist, 4) + " s" + Math.Round(sellDist, 4) + " after " + round + " rounds");
         }
 
-        //Todo: Optimize outcome % for (outcomePercent * ((buyDist + sellDist) / 2))
-        //Just set a maximum amount of rounds and get the result then.
+        private void optimizeOutcomeCodePercentage(int rounds)
+        {
+            Logger.log("Optimizing outcomecode percentage");
+            double successRatio;
+            double[][] outcomeMatrix = OutcomeGenerator.getOutcome(data, outcomeTimeframe, out successRatio);
+            outcomeCodePercent = 0.5;
 
+            if (successRatio < 0.9)
+                throw new Exception("Way too low success rate: " + successRatio);
+
+            double buyDist = double.NaN, sellDist = double.NaN;
+            double lastScore = double.MinValue;
+            double direction = -0.01;
+
+            int round;
+            for (round = 1;  round < rounds; round++)
+            {
+                double successRatioCode;
+                outcomeCodes = OutcomeGenerator.getOutcomeCode(data, outcomeMatrix, outcomeCodePercent, out successRatioCode);
+
+                if (successRatioCode < 0.9)
+                    throw new Exception("Too low success ratio: " + successRatioCode);
+
+                DistributionHelper.getOutcomeCodeDistribution(outcomeCodes, out buyDist, out sellDist);
+
+                double score = ((buyDist + sellDist) / 2) * outcomeCodePercent;
+                if (score < lastScore) //Wrong direction
+                {
+                    direction = direction * (-1);
+                    Logger.log("New opt. direction: " + direction);
+                }
+
+                if(outcomeCodePercent <= 0 && direction <= 0)
+                    direction = Math.Abs(direction);
+
+                outcomeCodePercent += (direction / (1 + (round / 20)));
+
+                Logger.log("PercDist OPT. Round " + round + " -> " + outcomeCodePercent + "% = |s"+ Math.Round(score, 4) + "| b" + Math.Round(buyDist, 4) + " s" + Math.Round(sellDist, 4));
+
+                lastScore = score;
+                round++;
+            }
+
+            Logger.log("SATTLE OPT. for " + outcomeCodePercent + "% at b" + Math.Round(buyDist, 4) + " s" + Math.Round(sellDist, 4) + " after " + round + " rounds");
+        }
+        
         private void testRandomIndicator()
         {
             //Create the random Indicator and retrive values
@@ -120,8 +166,6 @@ namespace V3_Trader_Project.Trader.Application
             //Retrive the max for buy and sell
             double maxBuy, maxSell;
             DistributionHelper.getSampleCodesMinMax(samplesMatrix, out maxBuy, out maxSell);
-            maxBuy -= 0.5;
-            maxSell -= 0.5;
 
             //Retrive the correalations
             double spBuy, spSell, pBuy, pSell;
