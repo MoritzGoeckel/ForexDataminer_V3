b6a4f69b52249656bae5f98b0e7e11360a951682
moritzgoeckel <moritz.goeckel@gmail.com>
2016-10-24 18:54:08 +0900

    Implemented the outcome sampling table visualizer

--- V3-Trader-Project/Trader/Tests/OutcomeSamplingVisualizer_Test.cs
+++ V3-Trader-Project/Trader/Tests/OutcomeSamplingVisualizer_Test.cs
@@ -37,7 +37,40 @@ namespace V3_Trader_Project.Trader.Tests
             samples[3] = null;
 
             Image img = OutcomeSamplingVisualizer.visualizeOutcomeCodeSamplingTable(samples, 500, 300, 0.3d);
-            ArrayVisualizer.showImg(img);
+            //ArrayVisualizer.showImg(img); //Todo: Test somehow
+        }
+
+        [TestMethod]
+        public void OutcomeSamplingTableVisualizer_Test()
+        {
+            double[] values = new double[100];
+            double[][] outcomes = new double[100][];
+            double[][] prices = new double[100][];
+            DateTime dt = DateTime.Now.ToUniversalTime();
+
+            Random z = new Random();
+            for (int i = 0; i < values.Length; i++)
+            {
+                //Min max actual
+                double d = z.NextDouble();
+                if (d > 0.7)
+                    outcomes[i] = new double[] { 1 - z.NextDouble() * 1, 1 + z.NextDouble() * 1, z.NextDouble() * 2 };
+                else
+                    outcomes[i] = new double[] { 1 - z.NextDouble() * 0.5, 1 + z.NextDouble() * 0.5, z.NextDouble() * 1 + 0.5 };
+
+                values[i] = d;
+
+                dt = dt.AddMilliseconds(1000);
+                prices[i] = new double[] { Timestamp.dateTimeToMilliseconds(dt), 1, 1, 0 };
+            }
+
+            double validOut;
+            double[][] samples = IndicatorSampler.sampleValuesOutcome(values, prices, outcomes, 0, 1, out validOut, 10);
+
+            samples[3] = null;
+
+            Image image = OutcomeSamplingVisualizer.visualizeOutcomeSamplingTable(samples, 500, 300, 0.3);
+            //ArrayVisualizer.showImg(image); //Todo: Test somehow
         }
     }
 }
--- V3-Trader-Project/Trader/Visualizers/OutcomeSamplingVisualizer.cs
+++ V3-Trader-Project/Trader/Visualizers/OutcomeSamplingVisualizer.cs
@@ -11,10 +11,103 @@ namespace V3_Trader_Project.Trader.Visualizers
 {
     public static class OutcomeSamplingVisualizer
     {
-        /*public static Image visualizeOutcomeSamplingTable(double[][] table, int width, int height)
+        public static Image visualizeOutcomeSamplingTable(double[][] table, int width, int height, double nowValue)
         {
+            Image img = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(img);
+            g.Clear(Color.LightGray);
 
-        }*/
+            double minValue = double.MaxValue, maxValue = double.MinValue;
+            double minSamples = double.MaxValue, maxSamples = double.MinValue;
+
+            double maxMax = double.MinValue;
+            double minMin = double.MaxValue;
+
+            //Find min max's
+            for (int rowId = 0; rowId < table.Length; rowId++)
+            {
+                if (table[rowId] != null && table[rowId][(int)SampleValuesOutcomeCodesIndices.SamplesCount] != 0)
+                {
+                    double value = table[rowId][(int)SampleValuesOutcomeIndices.Start];
+                    double samples = table[rowId][(int)SampleValuesOutcomeIndices.SamplesCount];
+
+                    double max = table[rowId][(int)SampleValuesOutcomeIndices.MaxAvg];
+                    double min = table[rowId][(int)SampleValuesOutcomeIndices.MinAvg];
+
+                    if (max > maxMax)
+                        maxMax = max;
+                    if (min < minMin)
+                        minMin = min;
+
+                    if (value > maxValue)
+                        maxValue = value;
+                    if (value < minValue)
+                        minValue = value;
+
+                    if (samples > maxSamples)
+                        maxSamples = samples;
+                    if (samples < minSamples)
+                        minSamples = samples;
+                }
+            }
+
+            Brush maxBrush = new SolidBrush(Color.Green);
+            Brush minBrush = new SolidBrush(Color.Blue);
+            Brush actualBrush = new SolidBrush(Color.Yellow);
+
+            double norm;
+            if (Math.Abs(minMin) > Math.Abs(maxMax))
+                norm = Math.Abs(minMin);
+            else
+                norm = Math.Abs(maxMax);
+
+            double columnSize = Convert.ToDouble(width) / table.Length;
+            for (int rowId = 0; rowId < table.Length; rowId++)
+            {
+                double x = columnSize * rowId;
+
+                if (table[rowId] == null || table[rowId][(int)SampleValuesOutcomeCodesIndices.SamplesCount] == 0)
+                {
+                    g.FillRectangle(new SolidBrush(Color.LightSalmon), Convert.ToInt32(x), 0, Convert.ToInt32(columnSize), height);
+                }
+                else
+                {
+                    double value = table[rowId][(int)SampleValuesOutcomeIndices.Start];
+                    double samples = table[rowId][(int)SampleValuesOutcomeIndices.SamplesCount];
+
+                    double min = table[rowId][(int)SampleValuesOutcomeIndices.MinAvg];
+                    double max = table[rowId][(int)SampleValuesOutcomeIndices.MaxAvg];
+                    double actual = table[rowId][(int)SampleValuesOutcomeIndices.ActualAvg];
+
+                    double samplesColor = ((samples - minSamples) / (maxSamples - minSamples)) * 255;
+
+                    double border = columnSize / 5d;
+
+                    //Draw the samples amount
+                    g.FillRectangle(new SolidBrush(Color.FromArgb(Convert.ToInt32(samplesColor), Convert.ToInt32(samplesColor), Convert.ToInt32(samplesColor))), Convert.ToInt32(x), 0, Convert.ToInt32(columnSize), height);
+
+                    double mid = height / 2d;
+
+                    //Min Max Actual
+                    g.FillRectangle(maxBrush, Convert.ToInt32(x), Convert.ToInt32(mid) - Convert.ToInt32(height / 2 * max / norm), Convert.ToInt32(columnSize), Convert.ToInt32(height / 2 * max / norm));
+                    g.FillRectangle(minBrush, Convert.ToInt32(x), Convert.ToInt32(mid), Convert.ToInt32(columnSize), Convert.ToInt32(height / 2 * Math.Abs(min) / norm));
+
+                    if(actual > 0)
+                        g.FillRectangle(actualBrush, Convert.ToInt32(x + border), Convert.ToInt32(mid) - Convert.ToInt32(height / 2 * actual / norm), Convert.ToInt32(columnSize - border * 2), Convert.ToInt32(height / 2 * Math.Abs(actual) / norm));
+                    else
+                        g.FillRectangle(actualBrush, Convert.ToInt32(x + border), Convert.ToInt32(mid), Convert.ToInt32(columnSize - border * 2), Convert.ToInt32(height / 2 * Math.Abs(actual) / norm));
+                }
+            }
+
+            if (double.IsNaN(nowValue) == false)
+            {
+                Pen currentValuePen = new Pen(Color.Red, 5);
+                int nowValueX = Convert.ToInt32(((nowValue - minValue) / (maxValue - minValue)) * Convert.ToDouble(width));
+                g.DrawLine(currentValuePen, nowValueX, 0, nowValueX, height);
+            }
+
+            return img;
+        }
 
         public static Image visualizeOutcomeCodeSamplingTable(double[][] table, int width, int height, double nowValue = double.NaN)
         {
@@ -55,7 +148,7 @@ namespace V3_Trader_Project.Trader.Visualizers
                 }
             }
 
-            Brush buyPen = new SolidBrush(Color.LightGreen);
+            Brush buyPen = new SolidBrush(Color.Green);
             Brush sellPen = new SolidBrush(Color.Blue);
 
             double columnSize = Convert.ToDouble(width) / table.Length;
